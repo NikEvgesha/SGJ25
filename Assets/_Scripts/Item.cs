74004a0553bef5c7b24ed14b10eb7a4505aa72fd
Melomaner <MelomanerGames@Gmail.com>
2025-11-03 21:04:50 +0300

    Ingridients

--- Assets/_Scripts/Item.cs
+++ Assets/_Scripts/Item.cs
@@ -3,5 +3,14 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [SerializeField] private ItemType _itemType;
+    private Ingredient _ingredient;
     public ItemType Type => _itemType;
+    public IngredientData Data => _ingredient.Data;
+
+    private void Awake()
+    {
+        _ingredient.GetComponent<Ingredient>();
+    }
+
+
 }
